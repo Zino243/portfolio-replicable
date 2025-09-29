855308825b3ca6f32589faf317d48b39da5b4848
Zino243 <bloodxs86@gmail.com>
2025-09-29 21:29:10 +0100

    yes

--- app/components/galeriaCard.tsx
+++ app/components/galeriaCard.tsx
@@ -3,8 +3,15 @@ import placeholder from "@/public/placeholder.png"
 import editImage from "@/public/pencil.svg"
 import papeleraSVG from "@/public/papelera.svg"
 import infoSVG from "@/public/info.svg"
+import { useState } from "react"
 
 export default function GaleriaCard() {
+  const actuallyDate = new Date();
+  const category = "en casa";
+
+  const data = actuallyDate.toString() + " \n" + category;
+  const [state, setState] = useState(true);
+
     return (
         <>
         <div className="bg-white rounded-lg shadow-md overflow-hidden">
@@ -23,16 +30,23 @@ export default function GaleriaCard() {
             <Image src={editImage} alt="Editar imagen" className="w-4 h-4" />
           </button>
           <div className="relative group">
-            {/* Elemento principal */}
-            <button title="holi" className="flex items-center text-gray-600 hover:text-gray-900">
-              <Image src={infoSVG} alt="Información" className="w-6 h-6" />
-            </button>
+          {/* Información */}
+          <button title={data} className="flex items-center text-gray-600 hover:text-gray-900">
+            <Image src={infoSVG} alt="Información" className="w-6 h-6" />
+          </button>
         </div>
         </div>
 
         <div className="flex items-center space-x-3">
-          <button className="bg-green-500 text-white rounded-full px-3 py-1 h-9 text-sm hover:bg-green-600">
-            Visible
+          <button
+            onClick={() => setState(!state)}
+            className={`transition text-white rounded-full px-3 py-1 h-9 text-sm 
+              ${state 
+                ? "bg-green-500 hover:bg-green-600" 
+                : "bg-gray-400 hover:bg-gray-500"}`
+            }
+          >
+            {state ? "Público" : "Privado"}
           </button>
 
           <button className="bg-red-600 p-2 rounded-full hover:bg-red-700">
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+"use client"
+
 export default function Home() {
+  const handleClick = () => {
+    window.location.href = "/admin/dashboard"; 
+  };
+
   return (
-    <h1>portfolio</h1>
+    <button
+      onClick={handleClick}
+      className="bg-red-500 text-white px-4 py-2 rounded"
+    >
+      Ir a otra ruta
+    </button>
   );
 }
